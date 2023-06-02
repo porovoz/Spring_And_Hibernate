b64d6f2d8d8024c2e5a09ea8ee773c297675dea6
porovoz <falkon1@list.ru>
2023-06-02 12:00:50 +0300

    initial commit

---
+++ src/main/java/com/bestapp/spring_and_hibernate/SpringAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.bestapp.spring_and_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAndHibernateApplication.class, args);
+    }
+
+}
